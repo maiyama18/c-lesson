9680e22170ceb00c2e7d15e98f9574907366f6db
maiyama18 <myam0118@gmail.com>
2021-08-13 09:53:06 +0900

    complete "01 intのパーサーを作ろう"

--- sources/forth_modoki/01_int_parser/int_parser.c
+++ sources/forth_modoki/01_int_parser/int_parser.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 static const char* const input = "123 456  1203";
 
+const char* parse_int(const char* c, int* parsed) {
+    while (*c && !isdigit(*c)) c++;
+    if (*c == '\0') {
+        fprintf(stderr, "unexpected end of string\n");
+        exit(1);
+    }
+
+    int v = 0;
+    while (*c && isdigit(*c)) {
+        v *= 10;
+        v += *c - '0';
+        c++;
+    }
+
+    *parsed = v;
+
+    return c;
+}
 
 int main() {
     int answer1 = 0;
     int answer2 = 0;
     int answer3 = 0;
 
-    // write something here.
-
+    const char* c = input;
+    c = parse_int(c, &answer1);
+    c = parse_int(c, &answer2);
+    parse_int(c, &answer3);
 
     // verity result.
     assert(answer1 == 123);
