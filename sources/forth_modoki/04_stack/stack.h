e50db6c59dd364f9183ee2dfbe0923475a714258
maiyama18 <myam0118@gmail.com>
2021-08-15 23:11:10 +0900

    define interface of stack

---
+++ sources/forth_modoki/04_stack/stack.h
@@ -0,0 +1,15 @@
+typedef enum {
+    NUMBER,
+    NAME,
+} LiteralType;
+
+typedef struct {
+    LiteralType type;
+    union {
+        int number;
+        char* name;
+    } u;
+} Literal;
+
+void stack_push(Literal);
+Literal* stack_pop();
