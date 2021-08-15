068609b0fcef1fd239199ba2929a5da7ec8f4ede
maiyama18 <myam0118@gmail.com>
2021-08-15 17:57:51 +0900

    add tests for parse_one

--- sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
+++ sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
@@ -13,9 +13,17 @@ typedef enum {
 
 int parse_one(int current_ch, int* out_val, TokenType* out_type);
 
+void test_parse_one_123();
+void test_parse_one_123_456();
+
 int main() {
     int answer1 = 0;
     int answer2 = 0;
+
+    test_parse_one_123();
+    test_parse_one_123_456();
+
+    cl_getc_set_src("123 456");
 
     TokenType type1, type2, types;
 
@@ -63,4 +71,40 @@ int parse_one(int current_ch, int* out_val, TokenType* out_type) {
     else {
         return 0;
     }
+}
+
+void test_parse_one_123() {
+    cl_getc_set_src("123");
+    int val;
+    TokenType token_type;
+
+    int current_ch = 0;
+    current_ch = parse_one(current_ch, &val, &token_type);
+
+    assert(token_type == NUMBER);
+    assert(val == 123);
+
+    assert(current_ch == EOF);
+}
+
+void test_parse_one_123_456() {
+    cl_getc_set_src("123 456");
+    int val1, val2, val3;
+    TokenType type1, type2, type3;
+
+    int current_ch = 0;
+    current_ch = parse_one(current_ch, &val1, &type1);
+    current_ch = parse_one(current_ch, &val2, &type2);
+    current_ch = parse_one(current_ch, &val3, &type3);
+
+    assert(type1 == NUMBER);
+    assert(val1 == 123);
+
+    assert(type2 == SPACE);
+    assert(val2 == 0);
+
+    assert(type3 == NUMBER);
+    assert(val3 == 456);
+
+    assert(current_ch == EOF);
 }
