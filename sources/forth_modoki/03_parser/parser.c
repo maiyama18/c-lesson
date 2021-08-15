9cbd1955d3831af5bd72e6a46a8e95a8f56bc882
maiyama18 <myam0118@gmail.com>
2021-08-15 19:41:15 +0900

    parse close curly

--- sources/forth_modoki/03_parser/parser.c
+++ sources/forth_modoki/03_parser/parser.c
@@ -88,6 +88,11 @@ int parse_one(int prev_ch, struct Token* out_token) {
         out_token->u.onechar = '{';
         return cl_getc();
     }
+    else if (prev_ch == '}') {
+        out_token->ltype = CLOSE_CURLY;
+        out_token->u.onechar = '}';
+        return cl_getc();
+    }
     else if (prev_ch == EOF) {
         out_token->ltype = END_OF_FILE;
         return EOF;
@@ -218,12 +223,29 @@ static void test_parse_one_open_curly() {
     assert(token.u.onechar == expect_char);
 }
 
+static void test_parse_one_close_curly() {
+    char* input = "}";
+    char expect_char = '}';
+    int expect_type = CLOSE_CURLY;
+
+    struct Token token = { UNKNOWN, {0} };
+    int ch;
+
+    cl_getc_set_src(input);
+    ch = parse_one(EOF, &token);
+
+    assert(ch == EOF);
+    assert(token.ltype == expect_type);
+    assert(token.u.onechar == expect_char);
+}
+
 static void unit_tests() {
     test_parse_one_empty_should_return_END_OF_FILE();
     test_parse_one_number();
     test_parse_one_executable_name();
     test_parse_one_literal_name();
     test_parse_one_open_curly();
+    test_parse_one_close_curly();
 }
 
 int main() {
