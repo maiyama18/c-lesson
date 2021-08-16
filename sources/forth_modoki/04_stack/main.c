e879cd1d5bc36d1de2f08cfd0b71f17945ddfb45
maiyama18 <myam0118@gmail.com>
2021-08-16 13:16:23 +0900

    rename verify method

--- sources/forth_modoki/04_stack/main.c
+++ sources/forth_modoki/04_stack/main.c
@@ -24,7 +24,7 @@ void assert_popped_literals_eq(Literal* expects[], int nexpects) {
     }
 }
 
-void verify_stack_pop(Literal inputs[], int ninputs, Literal* expects[], int nexpects) {
+void verify_stack_push_pop(Literal inputs[], int ninputs, Literal* expects[], int nexpects) {
     for (int i = 0; i < ninputs; i++) stack_push(inputs[i]);
     assert_popped_literals_eq(expects, nexpects);
 }
@@ -33,28 +33,28 @@ void test_stack_pop_empty() {
     Literal inputs[] = {};
     Literal* expects[] = { NULL };
 
-    verify_stack_pop(inputs, 0, expects, 1);
+    verify_stack_push_pop(inputs, 0, expects, 1);
 }
 
 void test_stack_pop_one_literal_number() {
     Literal inputs[] = { { NUMBER, {.number = 42} } };
     Literal* expects[] = { &inputs[0], NULL };
 
-    verify_stack_pop(inputs, 1, expects, 2);
+    verify_stack_push_pop(inputs, 1, expects, 2);
 }
 
 void test_stack_pop_one_literal_name() {
     Literal inputs[] = { { NAME, {.name = "foo"} } };
     Literal* expects[] = { &inputs[0], NULL };
 
-    verify_stack_pop(inputs, 1, expects, 2);
+    verify_stack_push_pop(inputs, 1, expects, 2);
 }
 
 void test_stack_pop_two_literals() {
     Literal inputs[] = { { NAME, {.name = "hello"} }, { NUMBER, {.number = -1} } };
     Literal* expects[] = { &inputs[1], &inputs[0], NULL };
 
-    verify_stack_pop(inputs, 2, expects, 3);
+    verify_stack_push_pop(inputs, 2, expects, 3);
 }
 
 int main() {
